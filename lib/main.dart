45bf31b096d90000ef2b27af698a49acedd4f3cd
I S L A M <islamashraf1539@gmail.com>
2024-09-02 01:31:43 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Portfolio());
+}
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
