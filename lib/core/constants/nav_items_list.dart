38c8f4273d842dd0a0c891996871e1c50e31f2f1
I S L A M <islamashraf1539@gmail.com>
2024-09-04 16:30:03 +0300

    Create End Drawer Mobile Widget with Nav Itemslist

---
+++ lib/core/constants/nav_items_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const List<String> navTitles = [
+  'Home',
+  'Skills',
+  'Projects',
+  'Contact',
+];
+
+const List<IconData> navIcons = [
+  Icons.home,
+  Icons.android_outlined,
+  Icons.apps_outlined,
+  Icons.contact_mail,
+];
---
+++ lib/features/home/presentation/widgets/end_drawer_mobile_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../../core/constants/nav_items_list.dart';
+import '../../../../core/utils/app_colors.dart';
+
+class EndDrawerMobileWidget extends StatelessWidget {
+  const EndDrawerMobileWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(AppColors.kPrimaryColor).withOpacity(0.8),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: ListView(
+          children: [
+            IconButton(
+              onPressed: () {
+                context.pop();
+              },
+              icon: const Icon(Icons.clear),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.all(20),
+            ),
+            for (int i = 0; i < navIcons.length; i++)
+              ListTile(
+                onTap: () {},
+                leading: Icon(
+                  navIcons[i],
+                  color: Colors.white,
+                ),
+                title: Text(
+                  navTitles[i],
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
