cd285d5c8ea6c8ba4c71f2bc147eb522af28672d
I S L A M <islamashraf1539@gmail.com>
2024-09-12 01:30:17 +0300

    Create My Project Details Widget&Used Project Card

---
+++ lib/features/home/presentation/widgets/my_projects_details_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:portfolio/features/home/presentation/widgets/custom_project_card_widget.dart';
+
+import '../../../../core/constants/nav_items_list.dart';
+
+class MyProjectsDetailsWidget extends StatelessWidget {
+  const MyProjectsDetailsWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 6,
+      padding:
+          EdgeInsets.symmetric(horizontal: checkIsMobileScreen() ? 75.w : 50.w),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: checkIsMobileScreen() ? 1 : 3,
+        childAspectRatio: checkIsMobileScreen() ? 0.7 : 1,
+        crossAxisSpacing: 10.w,
+        mainAxisSpacing: 20.h,
+      ),
+      itemBuilder: (context, index) => const CustomProjectCardWidget(),
+    );
+  }
+
+// To Check is it Mobile or Desktop Screen.
+  bool checkIsMobileScreen() => ScreenUtil().screenWidth <= kScreenWidth;
+}
