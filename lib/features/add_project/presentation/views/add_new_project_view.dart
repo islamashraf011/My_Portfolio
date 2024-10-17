86dccb33431930b17347c4f7997a2457ff5c3db0
I S L A M <islamashraf1539@gmail.com>
2024-10-17 06:05:07 +0300

    Provide LoginCubit in Add New Project View by Wrap it with Bloc Provider

--- lib/features/add_project/presentation/views/add_new_project_view.dart
+++ lib/features/add_project/presentation/views/add_new_project_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:portfolio/core/utils/app_colors.dart';
+import 'package:portfolio/features/add_project/presentation/manager/login_cubit/login_cubit.dart';
 import 'package:portfolio/features/add_project/presentation/widgets/add_new_project_view_body.dart';
 
 class AddNewProjectView extends StatelessWidget {
@@ -8,13 +10,16 @@ class AddNewProjectView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: ScreenUtil().screenWidth <= 690 ? 30.w : 100.w,
-        vertical: 50.h,
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: ScreenUtil().screenWidth <= 690 ? 30.w : 100.w,
+          vertical: 50.h,
+        ),
+        color: const Color(AppColors.kAppBarColor).withOpacity(0.7),
+        child: const AddNewProjectViewBody(),
       ),
-      color: const Color(AppColors.kAppBarColor).withOpacity(0.7),
-      child: const AddNewProjectViewBody(),
     );
   }
 }
