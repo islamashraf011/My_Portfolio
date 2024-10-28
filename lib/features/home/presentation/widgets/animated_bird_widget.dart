d790579b712e4cb02b1e8d495d8bd9a57117f82f
I S L A M <islamashraf1539@gmail.com>
2024-10-28 03:34:08 +0300

    Remove Lottie Package because not support web(html) and Used Rive Package but without stop animation after start.

--- lib/features/home/presentation/widgets/animated_bird_widget.dart
+++ lib/features/home/presentation/widgets/animated_bird_widget.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:lottie/lottie.dart';
+import 'package:rive/rive.dart';
 
 class AnimatedBirdWidget extends StatelessWidget {
   const AnimatedBirdWidget({super.key, required this.isMobile});
   final bool isMobile;
   @override
   Widget build(BuildContext context) {
-    return Lottie.asset(
-      'assets/animations/bird_animation.json',
-      repeat: false,
-      fit: BoxFit.fill,
+    return SizedBox(
       width: isMobile ? 1.sw : 130.w,
       height: isMobile ? 180.h : 1.sh,
+      child: const RiveAnimation.asset(
+        'assets/animations/bird_re_animated.riv',
+        fit: BoxFit.fill,
+      ),
     );
   }
 }
